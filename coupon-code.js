389e706011ee32d698a3d007110ce63651d00ddc
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 <sharath.yaragatti@yahoo.com>
2022-09-10 14:18:01 +0530

    ""

---
+++ coupon-code.js
@@ -0,0 +1,6 @@
+const couponCode= document.getElementById('coupon-code-div');
+//const couponcode=document.getElementById('')
+const couponbtn=document.getElementById('btn-coupon');        
+couponbtn.onclick= ()=>{
+  couponCode.classList.toggle('d-none');
+};
